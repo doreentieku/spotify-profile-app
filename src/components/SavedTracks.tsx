0875451d666bbaeae01480efd87c12958f6106b6
Doreen Ofosua-Tieku <dofosuatieku@gmail.com>
2026-03-27 17:57:08 -0500

    Fix pop colors

--- src/components/SavedTracks.tsx
+++ src/components/SavedTracks.tsx
@@ -193,9 +193,9 @@ export default function SavedTracks({
   };
 
   function getPopularityColor(popularity: number) {
-    if (popularity >= 70) return "bg-green-400";
-    if (popularity >= 40) return "bg-yellow-400";
-    return "bg-red-400";
+    if (popularity >= 70) return "bg-red-400";
+    if (popularity >= 40) return "bg-green-400";
+    return "bg-yellow-400";
   }
 
   return (
